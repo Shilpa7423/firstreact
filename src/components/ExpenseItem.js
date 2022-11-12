4dfc1b46c75e7b022c5b1a4124b7a5bee1e47726
Shilpa7423 <shilpa7423@gmail.com>
2022-11-13 01:35:26 +0530

    committing multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails.js";
+import "./ExpenseItem.css";
+
 
 function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-        <div>{props.location}</div>
-      </div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails Title={props.title} />
+      <ExpenseDetails Amount={props.amount} />
+      <ExpenseDetails location={props.location} />
     </div>
   );
 }
