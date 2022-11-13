e56e04d0b32b72755ad73773780822a534c56b26
Shilpa7423 <shilpa7423@gmail.com>
2022-11-13 22:56:00 +0530

    second commit multiple useState

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -25,15 +25,15 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onClick={titleChangeHandler} />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onClick={amountChangeHandler}  />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}  />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onClick={dateChangeHandler}  />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}  />
         </div>
       </div>
       <div className="new-expense__actions">
