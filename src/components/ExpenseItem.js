03098bca3b727c15aa3a2ec0ba65d32c318c18bb
Shilpa7423 <shilpa7423@gmail.com>
2022-11-13 13:36:37 +0530

    committing prop children(Card)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,26 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails.js";
 import "./ExpenseItem.css";
+import Card from './Card.js'
+
 
 
 function ExpenseItem(props) {
+ // return React.createElement('div',{},
+  //React.createElement(ExpenseDate,{date:props.date},{}),
+  //React.createElement(ExpenseDetails,{Title:props.title},{}),
+  //React.createElement(ExpenseDetails,{Amount:props.amount},{}),
+  //React.createElement(ExpenseDetails,{location:props.location},{}),
+    //  );
+  
   return (
-    <div className="expense-item">
+    < Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails Title={props.title} />
       <ExpenseDetails Amount={props.amount} />
       <ExpenseDetails location={props.location} />
-    </div>
+    </Card>
   );
 }
 
